d7ea77fa39c167db67439369e38984ce99c2b779
yuzutomato <kousanden@gmail.com>
2017-09-16 00:28:33 +0900

    TODOの完了機能を実装

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 $("#add").click(function () {
     var inputTodo = $("#input-todo").val();
-    $("#todo-list").append("<li>" + inputTodo + "</li>");
+    $("#todo-list").append("<li><input type='checkbox'>" + inputTodo + "</li>");
+});
+
+$(document).on("change", "input[type=checkbox]", function () {
+    if($(this).is(":checked")) {
+        $(this).parent().css("text-decoration", "line-through");
+    }
+    else{
+        $(this).parent().css("text-decoration", "none");
+    }
 });
